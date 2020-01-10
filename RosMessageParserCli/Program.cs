3b6732a786baf98f265d18ff3afb9c65d598d951
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2020-01-10 15:07:02 +0100

    Add CLI project

---
+++ RosMessageParserCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RosMessageParserCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
